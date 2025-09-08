3205820436472cddb1305c2d92621418da906344
Snehasahdev16 <sahdevsneha@gmail.com>
2025-09-08 12:19:23 +0530

    Cleanup project files and keep only useful ones

--- AMP-Email-Dashboard/src/main/java/com/AMP_Dashboard/AMP_Email_Dashboard/controller/GenerateResponse.java
+++
@@ -1,9 +0,0 @@
-package com.AMP_Dashboard.AMP_Email_Dashboard.controller;
-
-public class GenerateResponse {
-    private String ampHtml;
-
-    public String getAmpHtml() { return ampHtml; }
-    public void setAmpHtml(String ampHtml) { this.ampHtml = ampHtml; }
-}
-
